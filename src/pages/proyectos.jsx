5d189aeb32aed120732223cfb08ed232f1f81fcf
angel <angelriera1796@gmail.com>
2022-08-08 10:11:38 -0400

    next front

---
+++ src/pages/proyectos.jsx
@@ -0,0 +1,16 @@
+import Projects from '@/components/Home/Projects'
+import Layout from '@/components/Layout'
+import React from 'react'
+
+export default function proyectos() {
+  return (
+    <Layout>
+        
+        <Projects/>
+
+        <Projects/>
+
+
+    </Layout>
+  )
+}
